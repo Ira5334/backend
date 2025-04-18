836d2d7ef27a4f3f5ce4ebbaa7a2c0de42d7dc20
Iryna Panchenko <iryna.panchenko@lnu.edu.ua>
2025-04-18 15:07:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
+connectionLimit: 10,
+  connectTimeout: 20000,
 });
 
 // ====== МАРШРУТИ ======
