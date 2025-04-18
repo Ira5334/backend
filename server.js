c78ffa9bb8824b437756f1e4315688efb65a1026
Iryna Panchenko <iryna.panchenko@lnu.edu.ua>
2025-04-18 10:22:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 // Підключення до бази даних
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
