83bbcd6bf0b8b2e0002067a67f01e38c47be7909
Iryna Panchenko <iryna.panchenko@lnu.edu.ua>
2025-04-18 12:19:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -58,25 +58,3 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Сервер працює на порту ${PORT}`);
 });
-
-const shutdown = () => {
-  console.log('Отримано сигнал завершення, закриваємо з'єднання...');
-  server.close(() => {
-    console.log('Сервер HTTP закрито.');
-    // Закриваємо пул з'єднань MySQL
-    db.end((err) => {
-      console.log('Пул з\'єднань MySQL закрито.');
-      process.exit(err ? 1 : 0);
-    });
-  });
-  
-  // Якщо сервер не закривається протягом 10 секунд, примусово завершуємо
-  setTimeout(() => {
-    console.log('Не вдалося закрити з\'єднання вчасно, примусове завершення');
-    process.exit(1);
-  }, 10000);
-};
-
-// Підписуємося на сигнали завершення
-process.on('SIGTERM', shutdown);
-process.on('SIGINT', shutdown);
